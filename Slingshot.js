fedf35c25693405254a5f4e6fc941a7b52baf5e4
aarushi3214 <7236.stkabirdin@gmail.com>
2021-02-18 21:13:05 +0530

    making the catapult

--- Slingshot.js
+++ Slingshot.js
@@ -6,9 +6,14 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
-        this.pointB = pointB
+        this.sling1=loadImage("sling1.png");
+        this.sling2=loadImage("sling2.png");
+        this.sling3=loadImage("sling3.png");
+        this.pointB = pointB;
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        //World.add(world, this.sling1);
+        //World.add(world, this.sling2);
     }
 
     fly(){
@@ -19,9 +24,24 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
-        }
+            push();
+            if(pointA.x<220){
+                strokeWeight(4);
+                image(this.sling1,200,20);
+                image(this.sling2,170,20);
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30);
+                stroke("#301608");
+                line(pointA.x-25, pointA.y, pointB.x, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+            }
+            else{
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
+                image(this.sling3,pointA.x+25,pointA.y-10,15,30);
+                stroke("#301608");
+            }
+            pop();
+            }
     }
     
 }
